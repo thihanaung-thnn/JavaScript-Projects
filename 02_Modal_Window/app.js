d4d6afa3bd6bc8ad05aa71480b8c50766bdb3181
thihanaung-thnn <80887792+thihanaung-thnn@users.noreply.github.com>
2022-02-22 18:46:47 +0630

    add esc key function

--- 02_Modal_Window/app.js
+++ 02_Modal_Window/app.js
@@ -4,15 +4,26 @@ const btnCloseModal = document.querySelector(".close-modal");
 const btnsShowModal = document.querySelectorAll(".show-modal");
 
 // show modal
-for (let i = 0; i < 3; i++) {
-  btnsShowModal[i].addEventListener("click", function () {
-    modal.classList.toggle("hidden");
-    overlay.classList.toggle("hidden");
-  });
-}
+const showModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 
 // close modal
-btnCloseModal.addEventListener("click", function () {
-  modal.classList.toggle("hidden");
-  overlay.classList.toggle("hidden");
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// button pressed
+for (let i = 0; i < 3; i++) {
+  btnsShowModal[i].addEventListener("click", showModal);
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") closeModal();
 });
